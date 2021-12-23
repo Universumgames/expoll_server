593059062cdd2029ed209604d417f8e16e29c42c
Universumgames <programming@universegame.de>
2021-12-23 12:26:36 +0100

    fixed config hassle

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
-import * as def from "./config.default.json"
-// import * as dev from "./config.default.json"
+import nodeconfig from "config"
 
-export const config = def
+export const config = {
+    mailServer: nodeconfig.get<string>("mailServer"),
+    mailPort: nodeconfig.get<number>("mailPort"),
+    mailSecure: nodeconfig.get<boolean>("mailSecure"),
+    mailUser: nodeconfig.get<string>("mailUser"),
+    mailPassword: nodeconfig.get<string>("mailPassword"),
+    serverPort: nodeconfig.get<number>("serverPort"),
+    frontEndPort: nodeconfig.get<number>("frontEndPort")
+}
+console.log(config)
