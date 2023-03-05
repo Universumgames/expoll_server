c8875b9780fc180a8f0c228e18c538fa2ecbc7c7
Universumgames <programming@universegame.de>
2023-03-05 23:03:24 +0100

    fix ci bugs and warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val exposed_version : String by project
-val h2_version : String by project
+val exposed_version: String by project
+val h2_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 group = "net.mt32.expoll"
