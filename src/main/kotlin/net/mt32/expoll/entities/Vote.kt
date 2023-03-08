09b9eadcde01aa913b69c74f10575ef597461245
Universumgames <programming@universegame.de>
2023-03-09 00:15:21 +0100

    add vote endpoint, update documentation

--- src/main/kotlin/net/mt32/expoll/entities/Vote.kt
+++ src/main/kotlin/net/mt32/expoll/entities/Vote.kt
@@ -9,10 +9,11 @@ import net.mt32.expoll.tPollID
 import net.mt32.expoll.tUserID
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
-class Vote: DatabaseEntity {
+class Vote : DatabaseEntity {
     val id: Int
     val userID: tUserID
     val pollID: tPollID
@@ -35,12 +36,12 @@ class Vote: DatabaseEntity {
         this.votedFor = VoteValue.values()[voteRow[Vote.votedFor]]
     }
 
-    override fun save(){
+    override fun save() {
         transaction {
-            Vote.upsert (Vote.id) {
+            Vote.upsert(Vote.id) {
                 it[id] = this@Vote.id
                 it[userID] = this@Vote.userID
-                it[pollID]= this@Vote.pollID
+                it[pollID] = this@Vote.pollID
                 it[optionID] = this@Vote.optionID
                 it[votedFor] = this@Vote.votedFor.id
             }
@@ -51,14 +52,21 @@ class Vote: DatabaseEntity {
         return "Vote(id=$id, userID='$userID', pollID='$pollID', optionID=$optionID, votedFor=$votedFor)"
     }
 
-    companion object : Table("vote"){
-        val id = integer("id")
+    companion object : Table("vote") {
+        val id = integer("id").autoIncrement()
         val userID = varchar("userId", UUIDLength)
         val pollID = varchar("pollId", UUIDLength)
         val optionID = integer("optionId")
         val votedFor = integer("votedFor")
 
         override val primaryKey = PrimaryKey(id)
+
+        fun fromID(id: Int): Vote? {
+            return transaction {
+                val vote = Vote.select { Vote.id eq id }.firstOrNull()
+                return@transaction vote?.let { Vote(it) }
+            }
+        }
 
         fun fromUser(user: User): List<Vote> {
             return transaction {
@@ -78,6 +86,44 @@ class Vote: DatabaseEntity {
             return transaction {
                 val result = Vote.select { Vote.pollID eq pollID }
                 return@transaction result.map { Vote(it) }
+            }
+        }
+
+        fun fromUserPollOption(userID: tUserID, pollID: tPollID, optionID: tOptionID): Vote? {
+            return transaction {
+                val result =
+                    Vote.select {
+                        (Vote.userID eq userID) and
+                                (Vote.pollID eq pollID) and
+                                (Vote.optionID eq optionID)
+                    }
+                        .firstOrNull()
+                return@transaction result?.let { Vote(it) }
+            }
+        }
+
+        private fun newID(): Int {
+            return transaction {
+                var id = 0
+                do {
+                    id++
+                } while (fromID(id) != null)
+                return@transaction id
+            }
+        }
+
+        fun setVote(userID: tUserID, pollID: tPollID, optionID: tOptionID, votedFor: VoteValue): Vote {
+            return transaction {
+                val vote = fromUserPollOption(userID, pollID, optionID) ?: Vote(
+                    newID(),
+                    userID,
+                    pollID,
+                    optionID,
+                    votedFor
+                )
+                vote.votedFor = votedFor
+                vote.save()
+                return@transaction vote
             }
         }
     }
--- src/main/kotlin/net/mt32/expoll/routes/VoteRoutes.kt
+++ src/main/kotlin/net/mt32/expoll/routes/VoteRoutes.kt
@@ -1,9 +1,58 @@
 package net.mt32.expoll.routes
 
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import net.mt32.expoll.BasicSessionPrincipal
+import net.mt32.expoll.VoteValue
+import net.mt32.expoll.entities.Poll
+import net.mt32.expoll.entities.Vote
+import net.mt32.expoll.helper.ReturnCode
+import net.mt32.expoll.helper.getDataFromAny
 
-fun Route.voteRoutes(){
-    route("vote"){
-
+fun Route.voteRoutes() {
+    route("vote") {
+        post {
+            voteRoute(call)
+        }
     }
+}
+
+suspend fun voteRoute(call: ApplicationCall) {
+    val principal = call.principal<BasicSessionPrincipal>()
+    if (principal == null) {
+        call.respond(ReturnCode.INTERNAL_SERVER_ERROR)
+        return
+    }
+    val pollID = call.getDataFromAny("pollID")
+    val optionID = call.getDataFromAny("optionID")?.toIntOrNull()
+    val votedFor = call.getDataFromAny("votedFor")?.toIntOrNull()
+    val userID = call.getDataFromAny("userID")
+
+    if (pollID == null || optionID == null || votedFor == null) {
+        call.respond(ReturnCode.MISSING_PARAMS)
+        return
+    }
+
+    if (userID != null && !principal.admin) {
+        call.respond(ReturnCode.UNAUTHORIZED)
+        return
+    }
+
+    val poll = Poll.fromID(pollID)
+    val votedForReal = VoteValue.values().find { it.id == votedFor }
+    if (poll == null ||
+        votedForReal == null ||
+        !poll.options.map { it.id }.contains(optionID) ||
+        (votedForReal == VoteValue.MAYBE && !poll.allowsMaybe)
+    ) {
+        call.respond(ReturnCode.NOT_ACCEPTABLE)
+        return
+    }
+
+    val userIDToUse = userID ?: principal.userID
+
+    Vote.setVote(userIDToUse, pollID, optionID, votedForReal)
+    call.respond(ReturnCode.OK)
 }
