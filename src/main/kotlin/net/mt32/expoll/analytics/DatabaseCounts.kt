918b63c3fd3fbf84f9e386434e684c8fc75db201
Universumgames <programming@universegame.de>
2024-01-26 00:11:33 +0100

    add more analytics data

--- src/main/kotlin/net/mt32/expoll/analytics/DatabaseCounts.kt
+++ src/main/kotlin/net/mt32/expoll/analytics/DatabaseCounts.kt
@@ -1,9 +1,11 @@
 package net.mt32.expoll.analytics
 
 import kotlinx.serialization.Serializable
+import net.mt32.expoll.database.DatabaseFactory
 import net.mt32.expoll.entities.*
 import net.mt32.expoll.entities.notifications.APNDevice
-import org.jetbrains.exposed.sql.select
+import net.mt32.expoll.serializable.request.Platform
+import org.jetbrains.exposed.sql.or
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -15,18 +17,37 @@ data class DatabaseCounts(
     val authDeviceCount: Long,
     val sessionCount: Long,
     val appSessionCount: Long,
-    val notificationDeviceCount: Long
+    val notificationDeviceCount: Long,
+    val deletedUserCount: Long,
+    val oidcConnections: Long,
+    val totalDiskUsage: Long
 )
 
-fun AnalyticsStorage.getCounts(): DatabaseCounts{
+fun AnalyticsStorage.getCounts(): DatabaseCounts {
     return transaction {
         val userCount = User.selectAll().count()
         val voteCount = Vote.selectAll().count()
         val pollCount = Poll.selectAll().count()
         val authDeviceCount = Authenticator.selectAll().count()
         val sessionCount = Session.selectAll().count()
-        val appSessionCount = Session.select { Session.userAgent eq "ios App" }.count()
+        val appSessionCount = Session.selectAll()
+            .where {
+                (Session.platform eq Platform.IOS.name) or
+                        (Session.platform eq Platform.ANDROID.name)
+            }.count()
+        val deletedUserCount = User.selectAll().where { User.deleted neq null}.count()
         val notificationDevices = APNDevice.selectAll().count()
+        val oidcConnections = OIDCUserData.selectAll().count()
+        var totalDiskUsage = -1L
+        DatabaseFactory.runRawSQL("SELECT table_schema AS \"Database\", SUM(data_length + index_length) AS \"Size\" FROM information_schema.TABLES GROUP BY table_schema"){
+            while(it.next()){
+                val dbName = it.getString("Database")
+                val size = it.getLong("Size")
+                if(dbName == "expoll"){
+                    totalDiskUsage = size
+                }
+            }
+        }
         return@transaction DatabaseCounts(
             userCount,
             voteCount,
@@ -34,7 +55,10 @@ fun AnalyticsStorage.getCounts(): DatabaseCounts{
             authDeviceCount,
             sessionCount,
             appSessionCount,
-            notificationDevices
+            notificationDevices,
+            deletedUserCount,
+            oidcConnections,
+            totalDiskUsage
         )
     }
 }
