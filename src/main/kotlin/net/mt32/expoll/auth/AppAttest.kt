91c4de44c104038441c93a8b3bad75d6c109c087
Universumgames <programming@universegame.de>
2024-02-28 13:21:49 +0100

    temporarily disable app attest verification

--- src/main/kotlin/net/mt32/expoll/auth/AppAttest.kt
+++ src/main/kotlin/net/mt32/expoll/auth/AppAttest.kt
@@ -1,14 +1,21 @@
 package net.mt32.expoll.auth
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.decodeFromByteArray
 import net.mt32.expoll.helper.defaultJSON
 import java.io.FileInputStream
 import java.security.cert.CertificateFactory
 
 
 // TODO test functionality
-fun verifyAppAttest(attestJSON: String): Boolean {
+@OptIn(ExperimentalSerializationApi::class)
+fun verifyAppAttest(attestBase64: String): Boolean {
+    return true
+    val attestJSON = String(java.util.Base64.getDecoder().decode(attestBase64))
+    val attestCBOR: AppleAppAttest = Cbor.decodeFromByteArray(java.util.Base64.getDecoder().decode(attestBase64))
+    print(attestCBOR)
     val attest: AppleAppAttest = defaultJSON.decodeFromString(attestJSON)
 
     val fact: CertificateFactory = CertificateFactory.getInstance("X.509")
@@ -32,7 +39,8 @@ fun verifyAppAttest(attestJSON: String): Boolean {
 data class AppleAppAttest(
     val fmt: String,
     val attStmt: AttestStatement,
-    val authData: ByteArray
+    val authData: ByteArray,
+    //val signature: ByteArray
 )
 
 @Serializable
